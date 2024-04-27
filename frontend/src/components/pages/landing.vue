c2ef16bb6ba3e4a601965295354c4faae3869227
ShamsZayan <89603134+ShamsZayan@users.noreply.github.com>
2024-04-27 16:17:14 +0300

    backend

--- frontend/src/components/pages/landing.vue
+++ frontend/src/components/pages/landing.vue
@@ -1,14 +1,25 @@
-<script setup>
-
+<script >
+export default {
+  data() {
+    return {
+      name: 'Forecasting Floods, Safeguarding Futures.',
+      message: 'I like apples'
+    }
+  }
+};
 </script>
 
 <template>
-  <div style="height: 10vh; background-color: white; ">
- 
+  <div style="">
+    <img style="z-index: -3; width: 100%; opacity:0.5; position: absolute; top: 0; left:0" src="/e135931e-e775-49cd-9768-ac9e74cfe684.png">
+
+    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; ">
+      <img src="/sayllogo-removebg.png" alt="Your Logo" style="width: 500px; " />
+      <p>{{ name }}</p>
+    </div>
+    
   </div>
 </template>
 
 <style scoped>
-
-
 </style>
