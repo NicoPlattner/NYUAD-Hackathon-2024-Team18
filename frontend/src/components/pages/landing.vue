7ecb8d6af40ca5d2ff61d570d578d465a99e65ca
Nico Plattner <nicoplattner95@gmail.com>
2024-04-27 20:11:56 +0400

    small ui change

--- frontend/src/components/pages/landing.vue
+++ frontend/src/components/pages/landing.vue
@@ -14,8 +14,8 @@ export default {
     <img style="z-index: -3; width: 100%; opacity:0.5; position: absolute; top: 0; left:0" src="/e135931e-e775-49cd-9768-ac9e74cfe684.png">
 
     <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; ">
-      <img src="/sayllogo-removebg.png" alt="Your Logo" style="width: 500px; " />
-      <p style="margin-bottom: 50px;">{{ name }}</p>
+      <img src="/sayllogo-removebg.png" alt="Your Logo" style="width: 55rem; " />
+      <p style="margin-bottom: 50px; font-family: Roboto; font-size: 3rem; margin-top: -7rem">Forecasting Floods, Safeguarding Futures.</p>
     </div>
     <div style="display: flex; flex-direction: column; align-items: center; ">
 
@@ -28,7 +28,7 @@ export default {
         <li>Rising Sea Level: Rising sea level due to climate change is gradually increasing the baseline water level in coastal areas. </li>
       </ul >
       <h1>Features</h1>
-      <p ">Flood forecasting is the process of predicting the occurrence, magnitude, timing, and duration of floods in a specific area, often by analysing various hydrological, meteorological, and environmental factors using Quantum Computer. The primary goal of flood prediction is to deliver timely and accurate information to decision-makers, empowering them to take appropriate actions to mitigate the potential consequences of flooding on human lives, property, and the environment. </p>
+      <p>Flood forecasting is the process of predicting the occurrence, magnitude, timing, and duration of floods in a specific area, often by analysing various hydrological, meteorological, and environmental factors using Quantum Computer. The primary goal of flood prediction is to deliver timely and accurate information to decision-makers, empowering them to take appropriate actions to mitigate the potential consequences of flooding on human lives, property, and the environment. </p>
     </div>
     
   </div>
